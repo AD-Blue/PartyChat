f295d4f2fafb9bf359286e4b0973238d9af4c3a6
Aries Dimaranan <aasdimaranan@gmail.com>
2021-08-23 23:25:38 -0600

    Newly registered users are now auto-logged in

--- client/src/Views/Register.js
+++ client/src/Views/Register.js
@@ -5,17 +5,28 @@ import { UserContext } from '../Context/UserContext';
 
 export default function Register() {
 
-    const { setUserObject, setIsAuthenticated } = useContext(UserContext);
+    const { setUserObject, setIsAuthenticated, isAuthenticated } = useContext(UserContext);
 
     const [usernameReg, setUsernameReg] = useState('');
     const [passwordReg, setPasswordReg] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [regLabel, setLabelText] = useState(<p className='reg-label'></p>);
+    const [newRegistered, setNewRegistered] = useState(false);
 
     const clearFields = () => {
         setUsernameReg('');
         setPasswordReg('');
         setConfirmPassword('');
+    }
+
+    const signOut = async () => {
+        try {
+            await axios.get('/api/users/logout');
+            console.log('Attempting to log out');
+            window.location.reload(false);
+        } catch (err) {
+            console.log(`Error signing out: ${err}`);
+        }
     }
 
     const handleKeyPress = (e) => {
@@ -51,6 +62,10 @@ export default function Register() {
                 setUserObject(post.data.data._id);
                 setIsAuthenticated(true);
 
+                setNewRegistered(true);
+
+                setLabelText(<p className='reg-label'>Registered the user {usernameReg}</p>)
+
                 clearFields();
             }
             else if (passwordReg !== confirmPassword) {
@@ -70,25 +85,43 @@ export default function Register() {
     }
 
     return (
-        <div className='registerContainer'>
-            <h1>Register</h1>
+        <>
+            {!isAuthenticated && (
+                <div className='registerContainer'>
+                    <h1>Register</h1>
+                
+                    <label>Username</label>
+                    <input type="text" value={usernameReg} onChange={(e) => setUsernameReg(e.target.value)} />
+                
+                    <label>Password</label>
+                    <input type="password" value={passwordReg} onChange={(e) => setPasswordReg(e.target.value)} />
+                
+                    <label>Confirm Password</label>
+                    <input type="password" value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        onKeyDown={handleKeyPress}
+                    />
+                
+                    <button className='btn-form' onClick={register}>Submit</button>
+                
+                    {regLabel}
+                
+                </div>
+            )}
 
-            <label>Username</label>
-            <input type="text" value={usernameReg} onChange={(e) => setUsernameReg(e.target.value)} />
+            {isAuthenticated && newRegistered && (
+                <div className='registerContainer'>
+                    {regLabel}
+                </div>
+            )}
 
-            <label>Password</label>
-            <input type="password" value={passwordReg} onChange={(e) => setPasswordReg(e.target.value)} />
-
-            <label>Confirm Password</label>
-            <input type="password" value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                onKeyDown={handleKeyPress}
-            />
-
-            <button className='btn-form' onClick={register}>Submit</button>
-
-            {regLabel}
-
-        </div>
+            {isAuthenticated && !newRegistered && (
+                <div className='registerContainer'>
+                    <h1>You are already logged in</h1>
+                    <p>Please sign out and try again</p>
+                    <button className='btn-form' onClick={signOut}>Sign Out</button>
+                </div>
+            )}
+        </>
     )
 }
